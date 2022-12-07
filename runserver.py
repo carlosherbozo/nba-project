acacb74f3e1b31b8d02118615074f3682735b8f1
Carlos Herbozo <carlosherbozo@Carloss-Air.wireless.yale.internal>
2022-12-06 21:06:36 -0500

    running

--- runserver.py
+++ runserver.py
@@ -5,20 +5,10 @@ import sys
 from app import app, db
 
 def main():
-    '''Parses args and starts regserver'''
-    parser = ArgumentParser(description='The registrar application')
-    parser.add_argument('port', help='the port at which the server should listen')
-    args = parser.parse_args()
-    try:
-        port = int(args.port)
-    except ValueError:
-        print('Invalid port:', args.port, file=sys.stderr)
-        sys.exit(1)
-
     with app.app_context():
         try:
             db.create_all()
-            app.run(host='0.0.0.0', port=port, debug=True)
+            app.run(host='0.0.0.0', port=80, debug=True)
         except Exception as ex:
             print(ex, file=sys.stderr)
             sys.exit(1)
