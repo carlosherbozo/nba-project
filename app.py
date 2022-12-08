8a0577d747dd4ef6a6b6a8ba2cf7312e1808235f
John Jacob Go <johnjacobgo@gmail.com>
2022-12-07 19:28:53 -0500

    updated csv

--- app.py
+++ app.py
@@ -80,8 +80,9 @@ def player_search(input: dict):
         filters = []
         vals = []
         for key in input.keys():
-            filters.append(f"{key} LIKE ? ")
-            vals.append(input[key])
+            if len(input[key]) > 0:
+                filters.append(f"{key} LIKE ? ")
+                vals.append(input[key])
         if len(filters) != 0:
             stmt_str += ("WHERE " + " AND ".join(filters))
             return execute_query(stmt_str, vals)
